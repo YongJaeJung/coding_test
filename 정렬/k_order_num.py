0d3e2ef0b93014865971146b96510cb7e981dc0b
YongJaeJung <yjwlm10@naver.com>
2021-12-03 16:34:43 +0900

    20211203

--- 정렬/k_order_num.py
+++ 정렬/k_order_num.py
@@ -25,9 +25,3 @@ def solution(array, commands):
         answer.append(sorted_temp_array[order-1])
 
     return answer
-
-
-array = [1, 5, 2, 6, 3, 7, 4]
-commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
-
-print(solution(array, commands))
